f474156f8831bf26623389e50f7715e92ef77245
Miha <mstih@proton.me>
2025-05-08 10:12:37 +0200

    added a few comments about the code

--- huffman-block.py
+++ huffman-block.py
@@ -1,5 +1,6 @@
 import random
 
+# Stores the possible letters and their probabilities
 alphabet = {
     'a' : 0.05, 
     'b' : 0.10,
@@ -12,13 +13,12 @@ alphabet = {
 symbols = list(alphabet.keys())
 p = list(alphabet.values())
 
+# Length of the randomly generated sequence
 sequence_len = 1000
 
 #Generate the sequence
 sequence = random.choices(symbols, weights=p, k=sequence_len )
-print("####################################################\n")
 print(f'Random sequence: {"".join(sequence[:100])}...\n')
-print("####################################################\n")
 
 # Create all 36 possible 2-letter blocks
 all_blocks = []
@@ -26,11 +26,12 @@ for a in symbols:
     for b in symbols:
         all_blocks.append(a + b)
 
-
+#Split sequence to blocks
 blocks = []
 for i in range(0, len(sequence) - 1, 2):
     blocks.append(sequence[i] + sequence[i+1])
 
+# Count how many times the each block appears 
 block_count = {block : 0 for block in all_blocks}
 for block in blocks:
     if block in block_count:
@@ -68,6 +69,7 @@ codes = {}
 for item in huffman_list[0][1:]:
     codes[item[0]] = item[1]
 
+# Print all the blocks with their codes
 print("Huffman codes for blocks:")
 for k, v in codes.items():
     print(f"{k}: {v}")
@@ -93,6 +95,7 @@ for b in blocks:
 print("Encoded sequence/bitstream (first 200 bits): ", encoded_bits[:200], '...')
 
 #DECODING function
+# Params: sequence of encoded bits, codebook of all the possible blocks
 def decode(encoded_bits, codebook):
     reverse_codebook = {v: k for k, v in codebook.items()}
     decoded_blocks = []
@@ -110,9 +113,11 @@ decoded_sequence = []
 for block in decoded_blocks:
     decoded_sequence.extend(list(block))
 
+# Prints out the decoded sequence as array
 decoded_sequence_string = ''.join(decoded_sequence)    
 print("Decoded sequence (first 100 characters): ", decoded_sequence[:100])
 
+# Checks if the decoding was in fact successfull
 original_sequence_string = ''.join(sequence[:len(decoded_sequence)])
 if decoded_sequence_string == original_sequence_string:
     print("Decoding successfull!")
